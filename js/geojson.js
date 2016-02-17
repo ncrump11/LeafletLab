662d1729033804a8e914c3a5f49e4114cb00452f
ncrump11 <student@SHC-380-013.shc.wisc.edu>
2016-02-17 14:46:36 -0600

    Wed2/17

--- js/geojson.js
+++ js/geojson.js
@@ -8,9 +8,11 @@ function createMap(){
 	});
 
 	//add OSM base tilelayer
-	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
+    //https://leaflet-extras.github.io/leaflet-providers/preview/
+	L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
     getData(map);
 };
@@ -27,65 +29,44 @@ function onEachFeature(feature, layer) {
     };
 };
 
+function createPropSymbols(data, map){
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
  //retrieves data and places it on map
- function getData(map){
- 	$.ajax("data/MegaCities.geojson",{
- 		dataType: "json",
- 		success: function(response){
-
-			var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-
-                }
-            }).addTo(map);
-         //    L.geoJson(response, {
-         //        onEachFeature: onEachFeature
-         //    }).addTo(map);
-       		
-       		// L.geoJson(response, {
-         //        //use filter function to only show cities with 2015 populations greater than 20 million
-         //        filter: function(feature, layer) {
-         //            return feature.properties.Pop_2015 > 20;
-         //        }
-         //    }).addTo(map);
-			 //examine the data in the console to figure out how to construct the loop
-            // console.log(response)
-
-            // //create an L.markerClusterGroup layer
-            // var markers = L.markerClusterGroup();
-
-            // //loop through features to create markers and add to MarkerClusterGroup
-            // for (var i = 0; i < response.features.length; i++) {
-            //     var a = response.features[i];
-            //     //add properties html string to each marker
-            //     var properties = "";
-            //     for (var property in a.properties){
-            //         properties += "<p>" + property + ": " + a.properties[property] + "</p>";
-            //     };
-            //     var marker = L.marker(new L.LatLng(a.geometry.coordinates[1], a.geometry.coordinates[0]), { properties: properties });
-            //     //add a popup for each marker
-            //     marker.bindPopup(properties);
-            //     //add marker to MarkerClusterGroup
-            //     markers.addLayer(marker);
-            // }
-
-            // //add MarkerClusterGroup to map
-            // map.addLayer(markers);
-
-       }
+function getData(map){
+    //load the data
+    $.ajax("data/Coke.geojson", {
+        dataType: "json",
+        success: function(response){
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
+        }
     });
-}
+};
 
  $(document).ready(createMap);
+
+ //GOAL: Proportional symbols representing attribute values of mapped features
+//STEPS:
+//1. Create the Leaflet map--done (in createMap())
+//2. Import GeoJSON data--done (in getData())
+//3. Add circle markers for point features to the map--done (in AJAX callback)
+//4. Determine which attribute to visualize with proportional symbols
+//5. For each feature, determine its value for the selected attribute
+//6. Give each feature's circle marker a radius based on its attribute value

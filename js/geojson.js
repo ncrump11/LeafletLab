8569081c9ad408798969ed1b5e3fa45c74bc333c
ncrump11 <ncrump11@yahoo.com>
2016-02-23 19:58:42 -0600

    Activity 5 completed

--- js/geojson.js
+++ js/geojson.js
@@ -17,20 +17,34 @@ function createMap(){
     getData(map);
 };
 
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
+// function onEachFeature(feature, layer) {
+//     //no property named popupContent; instead, create html string with all properties
+//     var popupContent = "";
+//     if (feature.properties) {
+//         //loop to add feature property names and values to html string
+//         for (var property in feature.properties){
+//             popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+//         }
+//         layer.bindPopup(popupContent);
+//     };
+// };
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 50;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
 };
 
 function createPropSymbols(data, map){
     //create marker options
+
+    var attribute = "2010(gal)";
+
     var geojsonMarkerOptions = {
         radius: 8,
         fillColor: "#ff7800",
@@ -38,20 +52,30 @@ function createPropSymbols(data, map){
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
+
+
     };
 
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
+
+            var attValue = Number(feature.properties[attribute]);
+
+            //Step 6: Give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+            
+
             return L.circleMarker(latlng, geojsonMarkerOptions);
         }
+
     }).addTo(map);
 };
 
  //retrieves data and places it on map
 function getData(map){
     //load the data
-    $.ajax("data/Coke.geojson", {
+    $.ajax("data/GasPrices.geojson", {
         dataType: "json",
         success: function(response){
             //call function to create proportional symbols

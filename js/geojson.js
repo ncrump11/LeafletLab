89aa946ff1e44e8c544814c5f71cdd70700e7563
ncrump11 <ncrump11@yahoo.com>
2016-03-05 00:20:30 -0600

    Activity 6 completed- overlay

--- js/geojson.js
+++ js/geojson.js
@@ -2,36 +2,25 @@
 
 //instantiate leaflet Map
 function createMap(){
-	var map= L.map('map',{
-		center: [20, 0],
-		zoom: 2
-	});
+    var map= L.map('map',{
+        center: [20, 0],
+        zoom: 2
+    });
 
-	//add OSM base tilelayer
+    //add OSM base tilelayer
     //https://leaflet-extras.github.io/leaflet-providers/preview/
-	L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 }).addTo(map);
 
     getData(map);
 };
 
-// function onEachFeature(feature, layer) {
-//     //no property named popupContent; instead, create html string with all properties
-//     var popupContent = "";
-//     if (feature.properties) {
-//         //loop to add feature property names and values to html string
-//         for (var property in feature.properties){
-//             popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-//         }
-//         layer.bindPopup(popupContent);
-//     };
-// };
+
 //calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
     //scale factor to adjust symbol size evenly
-    var scaleFactor = 50;
+    var scaleFactor = 35;
     //area based on attribute value and scale factor
     var area = attValue * scaleFactor;
     //radius calculated based on area
@@ -40,36 +29,177 @@ function calcPropRadius(attValue) {
     return radius;
 };
 
-function createPropSymbols(data, map){
+function pointToLayer(feature, latlng, attributes){
     //create marker options
 
-    var attribute = "2010(gal)";
+    var attribute = attributes[0];
 
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "#ff7800",
-        color: "#000",
+
+    var options = {
+        // radius: 8,
+        fillColor: "#FF6347 ",
+        color: "#FFFFFF",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
 
-
     };
 
     //create a Leaflet GeoJSON layer and add it to the map
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
 
-            var attValue = Number(feature.properties[attribute]);
+        var attValue = Number(feature.properties[attribute]);
 
-            //Step 6: Give each feature's circle marker a radius based on its attribute value
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+         //Step 6: Give each feature's circle marker a radius based on its attribute value
+         options.radius = calcPropRadius(attValue);
+
+         //create circle marker layer
+         var layer = L.circleMarker(latlng, options);
+
+         
             
+        //build popup content
+         var panelContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
 
-            return L.circleMarker(latlng, geojsonMarkerOptions);
+         //add formatted attribute to panel content string
+         var year = attribute.split("(")[0];
+         panelContent += "<p><b>Gas price in " + year + ":</b> " + "$" + feature.properties[attribute].toFixed(2) + " per gallon</p>";
+         
+         //popup content is now just the city name
+         var popupContent = feature.properties.City;
+
+         //bind to circle marker
+        layer.bindPopup(panelContent, {
+            offset: new L.Point(0,-options.radius),
+            closeButton: true
+        });
+
+        //event listeners to open popup on hover
+        layer.on({
+            mouseover: function(){
+                this.openPopup();
+            },
+
+            mouseout: function(){
+                this.closePopup();
+            },
+            // click: function(){
+            //     $("#panel").html(panelContent);
+            //     closeButton: true
+            // }
+        });
+
+        //return the circle marker to the L.geoJson pointToLayer option
+        return layer;
+    
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
         }
-
     }).addTo(map);
+};
+
+
+
+//create sequence controls
+function createSequenceControls(map, attributes){
+    //create range input element (slider)
+    $('#panel').append('<input class="range-slider" type="range">');
+
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 5,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+
+    $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
+    $('#panel').append('<button class="skip" id="forward">Skip</button>');
+    $('#reverse').html('<img src="img/reverse.png">');
+    $('#forward').html('<img src="img/forward.png">');
+
+     //Step 5: click listener for buttons
+    $('.skip').click(function(){
+         //get the old index value
+        var index = $('.range-slider').val();
+
+         //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 5 ? 0 : index;
+
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 5 : index;
+        
+        };
+
+        //update slider
+        $('.range-slider').val(index);
+            //pass new attribute to update symbols
+            updatePropSymbols(map, attributes[index]);
+        
+    });
+    //input listener for slider
+    $('.range-slider').on('input', function(){
+        //get the new index value
+        var index = $(this).val();
+
+        //pass new attribute to update symbols
+        updatePropSymbols(map, attributes[index]);
+    });
+
+};
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("gal") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+};    
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+         if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>City:</b> " + props.City + "</p>";
+
+            //add formatted attribute to panel content string
+            var year = attribute.split("_")[0];
+            popupContent += "<p><b>Gas price in " + year + ":</b> " + "$" + props[attribute].toFixed(2) + " per gallon</p>";
+
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+    });
 };
 
  //retrieves data and places it on map
@@ -79,9 +209,51 @@ function getData(map){
         dataType: "json",
         success: function(response){
             //call function to create proportional symbols
-            createPropSymbols(response, map);
+            var attributes = processData(response);
+
+            createPropSymbols(response, map, attributes);
+            createSequenceControls(map, attributes);
+            
         }
     });
+    $.ajax("data/oil.geojson",{
+        dataType: "json",
+        success: function(response){
+        overlayOilData(response, map);
+        }
+    })
+};
+
+//fifth operator - overlay World's largest oil reserves
+//source: https://www.eia.gov/cfapps/ipdbproject/IEDIndex3.cfm?tid=5&pid=57&aid=6
+function overlayOilData(response, map){
+   
+            //create marker styles
+
+            var overlayOptions = {
+                radius: 8,
+                fillColor: "#6A5ACD ",
+                color: "#FFFFFF",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            var newLayer = L.geoJson(response, {
+
+                pointToLayer: function(feature,latlng){
+                    return L.circleMarker(latlng, overlayOptions);  
+                }
+
+            });
+
+
+            var oilLayer = {
+                "Largest Proven Oil Reserves" : newLayer
+            };
+
+            L.control.layers(null, oilLayer).addTo(map);
+
 };
 
  $(document).ready(createMap);
